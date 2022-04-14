c6c8b883c07e810ef6ac7e00adb03d243d1b0e9e
Choux <choifrance@gmail.com>
2022-04-15 02:36:07 +0900

    add new slice-reducer

--- starting-redux/src/store/index.js
+++ starting-redux/src/store/index.js
@@ -2,12 +2,13 @@
 import { createStore } from 'redux';
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true }
+const initialCounterState = { counter: 0, showCounter: true }
+const initialAuthState = { isAuthenticated: false }
 
 // redux-toolkit을 활용하여 리듀서 함수를 더욱 간단하게 작성하기
 const counterSlice = createSlice({
   name: 'counter',
-  initialState: initialState,
+  initialState: initialCounterState,
   // reducers에 작성한 함수의 이름이 곧 액션타입이 된다.
   reducers: {
     increment(state, action) {
@@ -24,8 +25,22 @@ const counterSlice = createSlice({
   }
 })
 
+// Auth에 관련된 리듀서 함수를 분리하여 작성하기
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true
+    },
+    logout(state) {
+      state.isAuthenticated = false
+    },
+  }
+})
+
 // 리듀서 함수를 정의하는 기본적인 방법, default state와 action 타입을 인자로 받는다.
-const counterReducer = (state = initialState, action) => {
+const counterReducer = (state = initialCounterState, action) => {
   // 액션타입에 따라 리턴값이 변경
   if (action.type === 'increment') {
     return {
@@ -58,12 +73,16 @@ const counterReducer = (state = initialState, action) => {
 // createSlice를 사용한 경우의 스토어 생성, 규모가 커지고 여러개의 slice가 생길 경우 reducer에 할당하는 객체에 slice들을 추가할 수 있다.
 // 전달한 reducer map을 configureStore가 하나로 병합하기 때문에 단일 스토어 규칙에 위배되지 않는다.
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer }
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  }
 })
 
 // createSlice 형태의 리듀서 함수에서 액션 객체를 생성하는 방법, 액션 생성자라고 한다.
 // actions에 메서드의 형태로 등록한 리듀서 함수들이 연결되며 액션타입의 이름은 메서드의 이름이다.
-export const counterActions = counterSlice.actions;
+export const counterActions = counterSlice.actions
+export const authActions = authSlice.actions
 
 // 생성한 store를 다른 컴포넌트에서 사용할 수 있도록 export
 export default store
