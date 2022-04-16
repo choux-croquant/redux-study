2a4c46e911549667de8eb950ffd068d24b7d6f81
Choux <choifrance@gmail.com>
2022-04-17 05:15:56 +0900

    use thunk function

--- redux-with-async/README.md
+++ redux-with-async/README.md
@@ -10,3 +10,5 @@ Udemy의 React 완벽 가이드 with Redux, Next.js, TypeScript 강좌를 학습
   + 쉽고 간단하지만, 해당 컴포넌트 내에서만 데이터를 다루며 전역적으로 사용할 수 없다.
 
 + Action creator내에서 다루는 방식
+  + thunk : action을 딜레이시키는 함수, thunk으로 action creator를 작성하는 것으로 action 자체를 반환하는 것이 아닌 action을 반환하는 한 단계 전의 함수를 반환하도록 하며, 이 사이에서 side-effect가 발생하는 작업들을 처리할 수 있다.
+  + thunk를 잘 활용하여 UI와 로직을 분리할 수 있다. 적절한 분리는 가독성을 높이고 디버깅을 용이하게 하기 때문에 고려해 보면 좋을 것.
--- redux-with-async/src/App.js
+++ redux-with-async/src/App.js
@@ -1,29 +1,93 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+// import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let isInitial = true
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
 
-  // useEffect내에서 비동기 요청을 처리하는 예시
+  useEffect(() => {
+    dispatch(fetchCartData())
+  }, [dispatch])
+
+  // (주석처리된 부분) useEffect내에서 비동기 요청을 처리하는 예시
   // 디펜던시인 cart의 변경이 일어날 때 side-effect를 처리한다.
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    }).then((res) => {
-      console.log(res)
-    })
-  }, [cart])
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    // thunk를 사용하여 비동기 요청을 처리하는 예시
+    // 비동기 처리 및 에러처리에 대한 로직을 action 생성자 내부로 이동시켜 분리할 수 있다.
+    // UI와 로직을 분리하여 컴포넌트를 보다 깔끔하게 유지할 수 있다.
+    if (cart.changed) {
+      dispatch(sendCartData(cart))
+    }
+
+    // const sendCartData = async () => {
+    //   dispatch(uiActions.showNotification(
+    //     {
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending cart data!',
+    //     }
+    //   ))
+
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/posts/1',
+    //     {
+    //       method: 'PUT',
+    //       body: JSON.stringify(cart),
+    //     }
+    //   )
+
+    //   if (!response.ok) {
+    //     throw new Error('Sending cart data failed')
+    //   }
+
+    //   dispatch(uiActions.showNotification(
+    //     {
+    //       status: 'success',
+    //       title: 'Success!',
+    //       message: 'Sent cart data successfully!',
+    //     }
+    //   ))
+
+    // }
+    // // 최초에 빈 카트 정보로 요청 보내는 것을 방지
+    // if (isInitial) {
+    //   isInitial = false
+    //   return
+    // }
+
+    // sendCartData().catch(err => {
+    //   dispatch(uiActions.showNotification(
+    //     {
+    //       status: 'error',
+    //       title: 'Error!',
+    //       message: 'Sent cart data failed!',
+    //     }
+    //   ))
+    // })
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification status={notification.status} title={notification.title} message={notification.message}/>}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
