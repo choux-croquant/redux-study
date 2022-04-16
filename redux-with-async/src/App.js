5b15eadf7f32f1563be6d5ec992be751113456fa
Choux <choifrance@gmail.com>
2022-04-16 23:53:20 +0900

    async work in useEffect

--- redux-with-async/src/App.js
+++ redux-with-async/src/App.js
@@ -1,11 +1,27 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const showCart = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+
+  // useEffect내에서 비동기 요청을 처리하는 예시
+  // 디펜던시인 cart의 변경이 일어날 때 side-effect를 처리한다.
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    }).then((res) => {
+      console.log(res)
+    })
+  }, [cart])
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
